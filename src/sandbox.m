f2b3ec4c599933a9ae6e51e9303ea3b270cb7091
Jason Locklin <jalockli@uwaterloo.ca>
2013-10-07 10:43:19 -0400

    # This is a combination of 2 commits. # The first commit's message is:
    Some of the tutorial + files unchanged from vandermeerlab
    
    # This is the 2nd commit message:
    
    Week 3 Assignment

--- src/sandbox.m
+++ src/sandbox.m
@@ -1,7 +1,7 @@
 set(0,'DefaultFigureWindowStyle','docked');
 
 % first, cd to where the data you just grabbed is located
-cd /home/jalockli/BIOL680/data/R042-2013-08-18/
+cd /home/jalockli/Documents/BIOL680/data/R042-2013-08-18/
 
 %% load the data (note, may need to unzip position data first)
 fc = FindFiles('*.t');
@@ -11,82 +11,160 @@ S = LoadSpikes(fc);
 
 [Timestamps, X, Y, Angles, Targets, Points, Header] = Nlx2MatVT('VT1.nvt', [1 1 1 1 1 1], 1, 1, [] );
 
-%%   ts and tsd objects: basic methods
-% verify csc object has timestamps and data
-if length(Data(csc)) == length(Data(csc));
-    fprintf('csc is good\n')
-else
-    fprintf('csc is bad\n')
-end
 
-minmax(Range(csc))
-csc_small = Restrict(csc, 5950, 6050);
-length(Data(csc_small)) 
+%% Week 2 assignment
 
-x = tsd((Timestamps*10^-6), X');
-y = tsd((Timestamps*10^-6), Y');
-x_small = Restrict(x, 5950, 6050);
-length(Data(x_small))
+% Creating a 100Hz signal
+fs1 = 2000;
+tvec = 0:1/fs1:4; % construct time axis, sampled at 2kHz
+ 
+freq1 = 100;
+y = sin(2*pi*freq1*tvec); % 100Hz signal
+ 
+ax1 = subplot(211);
+stem(tvec,y); title('original'); %Stem and circle plot
 
-%% Plotting with handles and figure callback function (navigate.m)
-f = figure('KeyPressFcn', @navigate);
-pl = plot(Range(csc_small), Data(csc_small));
+%Downsample (every fourth sample)
+subsample_factor = 10; %4 Produces a reasonable result, 10 results in garbage
+ 
+tvec2 = tvec(1:subsample_factor:end); % take every 4th sample
+y2 = y(1:subsample_factor:end);
+ 
+ax2 = subplot(212);
+stem(tvec2,y2,'r'); title('subsampled'); 
+xlabel('time (s)');
 
-set(gcf,'Color',[0 0 0]);
-set(gca, 'XColor', [1,1,1]);
-set(gca, 'YColor', [1,1,1]);
-set(gca, 'XLim', [5989, 5990], 'FontSize', 24);
+% Rescale axis
+xl = [1 1.04];
+set(ax1,'XLim',xl); set(ax2,'XLim',xl);
 
-hold on; box off;
-
-csc_mean = nanmean(Data(csc));
-xr = get(gca, 'XLim');
-mean_hdl = plot(xr, [csc_mean csc_mean]);
-set(mean_hdl, 'Color', [1,0,0]);
-set(mean_hdl, 'LineStyle', '--');
-set(mean_hdl, 'LineWidth', 2);
-
-%% Print it
-set(gcf, 'InvertHardcopy', 'off')
-print(gcf,'-dpng','-r300','R042-2013-08-18-LFPsnippet.png');
-
-%% Anonymous Functions
-sqr_fn = @(x) x.^2;
-sqr_fn(2)
-
-%% Neuroplot
-% S is spikes
-
-%if interactive:
-f = figure('KeyPressFcn', @navigate);
-
-spikes = [];
-yvalues = [];
-
-%Turn the data into two vectors (X and Y) for ploting.
-for yindex = 1:length(S)
-    spikes = vertcat(spikes, Data(S{yindex}));
-    yvalues = vertcat(yvalues, ones(length(Data(S{yindex})),1)*yindex );
-end
-
-% Scatterplot is very fast, but only has a small set of markers available
-% In another language, I would use |, but here, I'll just use a triangle.
-scatter(spikes, yvalues, '>');
-set(gca, 'XLim', [min(spikes) min(spikes)+2]);
-%set(gca,'box','off','ycolor',[0.5 0.5 0.5]);
-% set(gca, 'ytick', []);
-
-
-%LFPs
+% Recover origional signal
 hold on;
+ 
+y_interp = interp1(tvec2,y2,tvec,'linear');
+p1 = plot(tvec,y_interp,'b');
+ 
+y_interp2 = interp1(tvec2,y2,tvec,'nearest');
+p2 = plot(tvec,y_interp2,'g');
 
-%Calculate scaling factor for max range +-10
-cf = diff(minmax(Data(csc)))/20;
-bias = get(gca, 'YLim');
-bias = bias(2) + 10;
+% Not able to plot spline because of this error
+% Error using griddedInterpolant/subsref
+% LAPACK loading error:
+% dlopen: cannot load any more object with static
+% TLS
+% 
+% Error in interp1 (line 192)
+%         VqLite = F(Xqcol);
+ 
+legend([p1 p2],{'linear','nearest'},'Location','Northeast'); legend boxoff
 
-set(gca, 'YLim', get(gca, 'YLim')+ [0 20]);
-pl = plot(Range(csc), (Data(csc)/cf)+bias );
+%
+sin(2*pi) == 0 % ...right? The answer might surprise you.
+fprintf('Welcome to numerical computing!\n');
+
+%% Just at the Nyquist frequency
+figure(2)
+subsample_factor = 10;
+ 
+tvec2 = tvec(2:subsample_factor:end); % best case scenario -- can detect 100Hz signal
+y2 = y(2:subsample_factor:end);
+ 
+subplot(212)
+stem(tvec2,y2,'r');
+set(gca,'XLim',xl);
+
+%% 2kHz Fs, 100Hz signal with 450Hz signal superimposed
+figure(3)
+ 
+fs1 = 2000;
+tvec = 0:1/fs1:4;
+ 
+freq1 = 100;
+freq2 = 450; % note, above Nyquist frequency for our target subsampled Fs
+ 
+y = sin(2*pi*freq1*tvec) + 0.5.*sin(2*pi*freq2*tvec);
+ 
+subplot(211)
+stem(tvec,y)
+set(gca,'XLim',xl);
+
+%% ss -- we don't care about the 450Hz signal, but...
+subsample_factor = 4;
+ 
+tvec2 = tvec(1:subsample_factor:end);
+y2 = y(1:subsample_factor:end);
+ 
+subplot(212)          % ALIASING!!!
+stem(tvec2,y2,'r');
+set(gca,'XLim',xl);
+
+%% Doing it properly 
+subsample_factor = 4;
+ 
+tvec2 = decimate(tvec, subsample_factor);
+y2 = decimate(y, subsample_factor);
+
+subplot(212)          % No aliasing!!!
+stem(tvec2,y2,'r');
+set(gca,'XLim',xl);
+
+%% Neuralynx Data
+
+cd /home/jalockli/Documents/BIOL680/data/R016-2012-10-08
+fname = 'R016-2012-10-08-CSC03b.ncs';
+[Timestamps, ~, SampleFrequencies, NumberOfValidSamples, Samples, Header] = Nlx2MatCSC(fname, [1 1 1 1 1], 1, 1, []);
+
+%Don't expect to see spikes as the sampling rate is too slow to catch them.
+
+% 16 bitts with a range of +_ 1500uV. Precision is:
+1500 / ( 0.5 * (2^16) )
+
+% So, the precision, or smallest voltage change that can be resolved would
+% be about 0.05uV. ADBitVolts in hHeader is this value in Volts.
+
+%From the header:
+ADBitVolts = 4.57778e-008 
+
+%Samples is in AD values, to convert to Volts:
+% Assuming AD = 0 = Volts,
+% 
+
+Samples_V = Samples * ADBitVolts; % convert to Volts
+
+Timestamps_s = Timestamps * 10^(-6); % convert to Seconds
+
+%Total number of samples:
+length(Samples(:))
+
+%From the header: '-SamplingFrequency 2000' (us)
+%Total duration assuming perfect sampling schedule:
+
+sampling_Frequency = 2000 
+length(Samples(:)) * sampling_Frequency
+
+%Actual Time elapsed:
+Timestamps_s(end) - Timestamps_s(1)
+
+% Restricting data to rat running time
+run(FindFile('*keys.m'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
