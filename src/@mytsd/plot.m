c519e446226265c8a6cf3608d0371b810a6c52cd
Jason Locklin <jalockli@uwaterloo.ca>
2013-11-01 15:40:16 -0400

    Week 5 Tutorial

--- src/@mytsd/plot.m
+++
@@ -1,6 +0,0 @@
-function plot(tsd_in)
-% function plot(tsd_in)
-%
-% plots the data in a tsd object
- 
-plot(tsd_in.t,tsd_in.data);
--- src/testLoadCSC.m
+++
@@ -1,8 +0,0 @@
-
-csc = LoadCSC('R016-2012-10-08-CSC03b.ncs');
-
-%Any negative diffs?
-any(diff(Range(csc)) < 0)
-
-
-
